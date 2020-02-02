d66c866f5ca903ce22a649560a1af47097134f3c
Luiz Augusto Wendt <luizaugustow_1@msn.com>
2020-02-02 06:12:31 -0300

    created lungs screen

---
+++ Assets/Source/UI/LungFeedback.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using LUT.Events.Primitives;
+
+public class LungFeedback : MonoBehaviour
+{
+	[SerializeField]
+	public EventBool onGameEnd;
+
+	public Sprite happyLung = null;
+	public Sprite sadLung = null;
+	private Image[] lungs;
+	
+	private GameFlowController cachedGameFlow;
+	void Awake()
+	{
+		onGameEnd.Register(OnGameEnd);
+		cachedGameFlow = GameFlowController.Instance;
+		lungs = GetComponentsInChildren<Image>();
+	}
+
+	private void OnDestroy()
+	{
+		onGameEnd.Unregister(OnGameEnd);
+	}
+
+	public void OnGameEnd(bool won)
+	{
+		Debug.Log($"has {lungs.Length} lungs and {cachedGameFlow.currentLife} life");
+		for(int i = 0; i < lungs.Length; i++)
+		{
+			if(cachedGameFlow.currentLife > i)
+			{
+				lungs[i].sprite = happyLung;
+			}
+			else
+			{
+				lungs[i].sprite = sadLung;
+			}
+		}
+	}
+}
