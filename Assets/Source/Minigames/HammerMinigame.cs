3a60f96c2952cd3faadd385d187a021ef81e923d
Luiz Augusto Wendt <luizaugustow_1@msn.com>
2020-02-02 14:52:27 -0300

    hammer feedback

--- Assets/Source/Minigames/HammerMinigame.cs
+++ Assets/Source/Minigames/HammerMinigame.cs
@@ -7,207 +7,218 @@ using TMPro;
 public class HammerMinigame : GameEndController
 {
 
-    // Window of time that the user can hit the object
-    public float hitTimespan = 0.5f;
-    public float spawnRate = 2.0f;
-    public int objectsToSpawn = 6;
-    public int minObjectsToHit = 3;
+	// Window of time that the user can hit the object
+	public float hitTimespan = 0.5f;
+	public float spawnRate = 2.0f;
+	public int objectsToSpawn = 6;
+	public int maxErrors = 3;
 
-    [Header("References")]
-    public GameObject objectPrefab;
-    public GameObject linePrefab;
-    public Transform objectSpawnLocation;
-    public ParticleSystem hitzonePS;
+	[Header("References")]
+	public GameObject objectPrefab;
+	public GameObject linePrefab;
+	public Transform objectSpawnLocation;
+	public ParticleSystem hitzonePS;
 
-    [Header("Hammer Sprites")]
-    public Image hammerImage;
-    public Sprite hammerDownSprite;
-    public Sprite hammerUpSprite;
+	[Header("Hammer Sprites")]
+	public Image hammerImage;
+	public Sprite hammerDownSprite;
+	public Sprite hammerUpSprite;
 
-    public Sprite toySprite;
-
-    private bool isBlowing = false;
-    private bool objectCanBeHit = false;
-    // Timer for next spawned object
-    private float nextSpawnTimer = 0.0f;
-    private int objectsSpawned = 0;
-    [SerializeField]
-    private int objectsHit = 0;
-    [SerializeField]
-    private int objectsMissed = 0;
-    private float timeToNextSpawn = 2.0f;
-
-    private List<GameObject> gameObjectsInHitzone = new List<GameObject>();
-
-    public Transform linesParent;
-    private float nextSpawnTimerx3;
-    private float spawnRatex3;
-
-    new private void Start()
-    {
-        base.Start();
-
-        nextSpawnTimerx3 = nextSpawnTimer / 5f;
-        spawnRatex3 = spawnRate / 5f;
-
-        WarmupRoller();
-        GetNextSpawnTime();
-    }
-
-    private void GetNextSpawnTime()
-    {
-        timeToNextSpawn = spawnRate + Random.Range(-spawnRate * 0.5f, spawnRate * 1.5f);
-    }
-
-    private void WarmupRoller()
-    {
-        int iterationsPerTimer = Mathf.CeilToInt(spawnRatex3 / Time.fixedDeltaTime);
-        for (int i = -1; i < 20; i++)
-        {
-            Vector3 offset = Vector3.down * linePrefab.GetComponent<HammerObjectMovement>().speed * Time.fixedDeltaTime * i * iterationsPerTimer;
-            //Debug.Log(offset);
-            Instantiate(linePrefab, objectSpawnLocation.position + offset, Quaternion.identity, linesParent);
-        }
-    }
-
-    private void Update()
-    {
-        if (!isGamePaused || IsCoutdownAlive())
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                HitHammer();
-            }
+	public Sprite toySprite;
 
 
-            if (Input.GetKeyUp(KeyCode.Space))
-            {
-                LiftHammer();
-            }
+	[Header("Ui")]
+	public Transform errorsContainer = null;
+	public GameObject errorsPrefab = null;
 
-            nextSpawnTimer += Time.deltaTime;
-            nextSpawnTimerx3 += Time.deltaTime;
+	private bool isBlowing = false;
+	private bool objectCanBeHit = false;
+	// Timer for next spawned object
+	private float nextSpawnTimer = 0.0f;
+	private int objectsSpawned = 0;
+	[SerializeField]
+	private int objectsHit = 0;
+	[SerializeField]
+	private int objectsMissed = 0;
+	private float timeToNextSpawn = 2.0f;
 
-            if (nextSpawnTimerx3 >= spawnRatex3)
-            {
-                nextSpawnTimerx3 = 0.0f;
-                Instantiate(linePrefab, objectSpawnLocation.position, Quaternion.identity, linesParent);
-            }
+	private List<GameObject> gameObjectsInHitzone = new List<GameObject>();
 
-            if (nextSpawnTimer >= timeToNextSpawn)
-            {
-                if (objectsSpawned < objectsToSpawn)
-                {
-                    SpawnObject();
-                }
-                else // If all objects have already been spawned
-                {
-                    CheckGameEnded();
-                }
-            }
+	public Transform linesParent;
+	private float nextSpawnTimerx3;
+	private float spawnRatex3;
 
-        }
-    }
+	new private void Start()
+	{
+		base.Start();
 
-    private bool IsCoutdownAlive()
-    {
-        return FindObjectOfType<Countdown>() != null;
-    }
+		nextSpawnTimerx3 = nextSpawnTimer / 5f;
+		spawnRatex3 = spawnRate / 5f;
 
-    private void CheckGameEnded()
-    {
-        if ((objectsHit + objectsMissed) == objectsToSpawn)
-        {
-            if (objectsHit >= minObjectsToHit)
-            {
-                OnWin();
-            }
-            else
-            {
-                OnLose();
-            }
-        }
-    }
+		for (int i = 0; i < maxErrors; i++)
+		{
+			Instantiate(errorsPrefab, errorsContainer);
+		}
 
-    private void SpawnObject()
-    {
-        if (IsCoutdownAlive())
-        {
-            return;
-        }
+		WarmupRoller();
+		GetNextSpawnTime();
+	}
 
-        Instantiate(objectPrefab, objectSpawnLocation.position, Quaternion.identity, objectSpawnLocation);
+	private void GetNextSpawnTime()
+	{
+		timeToNextSpawn = spawnRate + Random.Range(-spawnRate * 0.5f, spawnRate * 1.5f);
+	}
 
-        GetNextSpawnTime();
-        nextSpawnTimer = 0.0f;
-        objectsSpawned++;
+	private void WarmupRoller()
+	{
+		int iterationsPerTimer = Mathf.CeilToInt(spawnRatex3 / Time.fixedDeltaTime);
+		for (int i = -1; i < 20; i++)
+		{
+			Vector3 offset = Vector3.down * linePrefab.GetComponent<HammerObjectMovement>().speed * Time.fixedDeltaTime * i * iterationsPerTimer;
+			//Debug.Log(offset);
+			Instantiate(linePrefab, objectSpawnLocation.position + offset, Quaternion.identity, linesParent);
+		}
+	}
 
-    }
+	private void Update()
+	{
+		if (!isGamePaused || IsCoutdownAlive())
+		{
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				HitHammer();
+			}
 
-    private void HitHammer()
-    {
-        if (isGamePaused) return;
-        // Set hammer to hit position
-        hammerImage.sprite = hammerDownSprite;
-        hitzonePS.Play();
 
-        while (gameObjectsInHitzone.Count > 0)
-        {
-            //Destroy(gameObjectsInHitzone[0]);
+			if (Input.GetKeyUp(KeyCode.Space))
+			{
+				LiftHammer();
+			}
 
-            gameObjectsInHitzone[0].GetComponent<Image>().sprite = toySprite;
+			nextSpawnTimer += Time.deltaTime;
+			nextSpawnTimerx3 += Time.deltaTime;
 
-            gameObjectsInHitzone.RemoveAt(0);
-            objectsHit++;
-        }
-    }
+			if (nextSpawnTimerx3 >= spawnRatex3)
+			{
+				nextSpawnTimerx3 = 0.0f;
+				Instantiate(linePrefab, objectSpawnLocation.position, Quaternion.identity, linesParent);
+			}
 
-    private void LiftHammer()
-    {
-        if (isGamePaused) return;
-        // Set hammer back lifted
-        hammerImage.sprite = hammerUpSprite;
-    }
+			if (nextSpawnTimer >= timeToNextSpawn)
+			{
+				if (objectsSpawned < objectsToSpawn)
+				{
+					SpawnObject();
+				}
+				else // If all objects have already been spawned
+				{
+					CheckGameEnded();
+				}
+			}
 
-    public override void OnBlowStatusChange(bool state)
-    {
-        // Start blowing is a hit
-        if (!isBlowing && state) { HitHammer(); }
+		}
+	}
 
-        // Stop blowing turns hammer back up
-        if (isBlowing && !state) { LiftHammer(); }
+	private bool IsCoutdownAlive()
+	{
+		return FindObjectOfType<Countdown>() != null;
+	}
 
-        isBlowing = state;
-    }
+	private void CheckGameEnded()
+	{
+		if (objectsMissed >= maxErrors)
+		{
+			OnLose();
+		}
+		else if ((objectsHit + objectsMissed) == objectsToSpawn)
+		{
+			OnWin();
+		}
+	}
 
-    public override void OnLose()
-    {
-        base.OnLose();
-    }
+	private void SpawnObject()
+	{
+		if (IsCoutdownAlive())
+		{
+			return;
+		}
 
-    public override void OnWin()
-    {
-        base.OnWin();
-    }
+		Instantiate(objectPrefab, objectSpawnLocation.position, Quaternion.identity, objectSpawnLocation);
 
-    public void OnObjectEnterHitzone(GameObject go)
-    {
-        //Debug.Log("ENTERED");
-        gameObjectsInHitzone.Add(go);
-    }
+		GetNextSpawnTime();
+		nextSpawnTimer = 0.0f;
+		objectsSpawned++;
 
-    public void OnObjectLeaveHitzone(GameObject go)
-    {
+	}
 
-        // Only if GO still exist (it can leave the trigger when it's destroyed)
-        if (gameObjectsInHitzone.Find(el => el == go))
-        {
-            //Debug.Log("LEFT");
-            objectsMissed++;
-            gameObjectsInHitzone.Remove(go);
-        }
+	private void HitHammer()
+	{
+		if (isGamePaused) return;
+		// Set hammer to hit position
+		hammerImage.sprite = hammerDownSprite;
+		hitzonePS.Play();
 
-        // Effect of losing object?
-    }
+		while (gameObjectsInHitzone.Count > 0)
+		{
+			//Destroy(gameObjectsInHitzone[0]);
+
+			gameObjectsInHitzone[0].GetComponent<Image>().sprite = toySprite;
+
+			gameObjectsInHitzone.RemoveAt(0);
+			objectsHit++;
+		}
+	}
+
+	private void LiftHammer()
+	{
+		if (isGamePaused) return;
+		// Set hammer back lifted
+		hammerImage.sprite = hammerUpSprite;
+	}
+
+	public override void OnBlowStatusChange(bool state)
+	{
+		// Start blowing is a hit
+		if (!isBlowing && state) { HitHammer(); }
+
+		// Stop blowing turns hammer back up
+		if (isBlowing && !state) { LiftHammer(); }
+
+		isBlowing = state;
+	}
+
+	public override void OnLose()
+	{
+		base.OnLose();
+	}
+
+	public override void OnWin()
+	{
+		base.OnWin();
+	}
+
+	public void OnObjectEnterHitzone(GameObject go)
+	{
+		//Debug.Log("ENTERED");
+		gameObjectsInHitzone.Add(go);
+	}
+
+	public void OnObjectLeaveHitzone(GameObject go)
+	{
+		if (isGamePaused || IsCoutdownAlive()) return;
+
+
+		// Only if GO still exist (it can leave the trigger when it's destroyed)
+		if (gameObjectsInHitzone.Find(el => el == go))
+		{
+			errorsContainer.GetChild(objectsMissed).GetChild(0).gameObject.SetActive(true);
+			//Debug.Log("LEFT");
+			objectsMissed++;
+			gameObjectsInHitzone.Remove(go);
+			CheckGameEnded();
+		}
+
+		// Effect of losing object?
+	}
 
 }
