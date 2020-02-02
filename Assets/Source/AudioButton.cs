d68b449b16c7d9241828ba13c1e87be4bcd92860
Luiz Augusto Wendt <luizaugustow_1@msn.com>
2020-02-02 16:33:37 -0300

    added sound to buttons

---
+++ Assets/Source/AudioButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button)), RequireComponent(typeof(AudioSource))]
+public class AudioButton : MonoBehaviour
+{
+    AudioSource source;
+    private void Awake()
+    {
+        GetComponent<Button>().onClick.AddListener(OnClick);
+        source = GetComponent<AudioSource>();
+    }
+
+    private void OnClick()
+    {
+        source.Play();
+    }
+}
